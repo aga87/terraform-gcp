f4afd1bb3ece2f05392b188c75ec889da68c736d
aga <agnieszka@mistergreen.nl>
2025-01-24 16:01:38 +0100

    Deploy static Next.js website

--- simple-infra/main.tf
+++ simple-infra/main.tf
@@ -6,6 +6,11 @@ resource "google_storage_bucket" "my_bucket" {
 
   uniform_bucket_level_access = false # Optional: to manage bucket permissions uniformly
 
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+
   # Optional: If you need to set lifecycle rules, versioning, etc.
   versioning {
     enabled = false
@@ -16,17 +21,20 @@ resource "google_storage_bucket" "my_bucket" {
   }
 }
 
-# Upload the html file to the bucket
-resource "google_storage_bucket_object" "my_website" {
-  name   = "index.html"
-  source = "../website/index.html"
-  bucket = google_storage_bucket.my_bucket.name
+# Upload all static files from the "out" folder to the bucket
+resource "google_storage_bucket_object" "my_website_files" {
+  for_each = fileset("../website-next-static/out", "**/*") # Upload all files from the "out" folder
+  name     = each.value
+  source   = "../website-next-static/out/${each.value}"
+  bucket   = google_storage_bucket.my_bucket.name
 }
 
-# Make new object (website html) public
+
+# Make all uploaded files publicly accessible
 resource "google_storage_object_access_control" "public_rule" {
-  bucket = google_storage_bucket.my_bucket.name
-  object = google_storage_bucket_object.my_website.name
-  role   = "READER"
-  entity = "allUsers"
+  for_each = google_storage_bucket_object.my_website_files
+  bucket   = google_storage_bucket.my_bucket.name
+  object   = each.value.name
+  role     = "READER"
+  entity   = "allUsers"
 }
--- website-next-static/next.config.ts
+++ website-next-static/next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/photo-portfolio-bucket/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
