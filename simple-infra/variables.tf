983e9d7265bbf65c5aaf84cfcb519dbfe8c72ad6
aga <agnieszka@mistergreen.nl>
2025-01-24 14:20:20 +0100

    Configure Google provider with project, region, and service account credentials

---
+++ simple-infra/variables.tf
@@ -0,0 +1,10 @@
+# We can assign these vars during running Terraform CLI command or we can provide them in the terraform.tfvars file
+
+variable "gcp_svc_key" {
+}
+
+variable "gcp_project" {
+}
+
+variable "gcp_region" {
+}
